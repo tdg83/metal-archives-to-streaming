27897a47cd39deef976b145b472bb01fafa7d90c
tdg83 <102386289+tdg83@users.noreply.github.com>
2025-04-14 16:35:00 +0200

    Add files via upload

--- background.js
+++ background.js
@@ -1,20 +1,85 @@
-browser.contextMenus.create({
-  id: "metalarchives-to-ytmusic",
-  title: "Search on YT Music",
-  contexts: ["link"]
+browser.contextMenus.removeAll().then(() => {
+  browser.contextMenus.create({
+    id: "ytmusic",
+    title: "Search on YouTube Music",
+    contexts: ["link"],
+    documentUrlPatterns: ["*://*.metal-archives.com/*"],
+	icons: {
+		"16": "streaming_icons/ytmusic16.png",
+		"32": "streaming_icons/ytmusic32.png"
+	}
+  });
+
+  browser.contextMenus.create({
+    id: "spotify",
+    title: "Search on Spotify",
+    contexts: ["link"],
+    documentUrlPatterns: ["*://*.metal-archives.com/*"],
+	icons: {
+		"16": "streaming_icons/spotify16.png",
+		"32": "streaming_icons/spotify32.png"
+	}
+  });
+  
+  browser.contextMenus.create({
+    id: "bandcamp",
+    title: "Search on Bandcamp",
+    contexts: ["link"],
+    documentUrlPatterns: ["*://*.metal-archives.com/*"],
+	icons: {
+		"16": "streaming_icons/bandcamp16.png",
+		"32": "streaming_icons/bandcamp32.png"
+	}
+  });
+});
+
+browser.contextMenus.onShown.addListener((info, tab) => {
+  const match = info.linkUrl?.match(/:\/\/(?:www\.)?metal-archives\.com\/albums\/([^/]+)\/([^/?#]+)/);
+
+  if (match) {
+    const band = decodeURIComponent(match[1]).replace(/_/g, " ");
+    const album = decodeURIComponent(match[2]).replace(/_/g, " ");
+
+    browser.contextMenus.update("ytmusic", {
+      title: `"${band} - ${album}" on YouTube Music`
+    });
+    browser.contextMenus.update("spotify", {
+      title: `"${band} - ${album}" on Spotify`
+    });
+	browser.contextMenus.update("bandcamp", {
+      title: `"${band} - ${album}" on Bandcamp`
+    });
+  } else {
+    browser.contextMenus.update("ytmusic", {
+      title: "Unvalid release link selected"
+    });
+    browser.contextMenus.update("spotify", {
+      title: "Unvalid release link selected"
+    });
+  }
+
+  browser.contextMenus.refresh();
 });
 
 browser.contextMenus.onClicked.addListener((info, tab) => {
-  if (info.menuItemId === "metalarchives-to-ytmusic") {
-    const url = info.linkUrl;
+  const url = info.linkUrl;
+  const match = url.match(/:\/\/(?:www\.)?metal-archives\.com\/albums\/([^/]+)\/([^/?#]+)/);
 
-    const match = url.match(/metal-archives\.com\/albums\/([^/]+)\/([^/?#]+)/);
-    if (match) {
-      const band = decodeURIComponent(match[1]).replace(/_/g, " ");
-      const album = decodeURIComponent(match[2]).replace(/_/g, " ");
-      const query = encodeURIComponent(`${band} ${album}`);
-      const searchUrl = `https://music.youtube.com/search?q=${query}`;
+  if (match) {
+    const band = decodeURIComponent(match[1]).replace(/_/g, " ");
+    const album = decodeURIComponent(match[2]).replace(/_/g, " ");
+    const query = encodeURIComponent(`${band} ${album}`);
 
+    let searchUrl;
+    if (info.menuItemId === "ytmusic") {
+      searchUrl = `https://music.youtube.com/search?q=${query}`;
+    } else if (info.menuItemId === "spotify") {
+      searchUrl = `https://open.spotify.com/search/${query}`;
+	} else if (info.menuItemId === "bandcamp") {
+      searchUrl = `https://bandcamp.com/search?q=${query}`;
+    }
+
+    if (searchUrl) {
       browser.tabs.create({ url: searchUrl });
     }
   }
